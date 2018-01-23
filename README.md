c68ed93d572d19eed3ce7ee56e1e908e695b28c4
iamthemzi <muhammadzilahee@gmail.com>
2018-01-23 17:57:24 +0600

    Updated comments

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 Neural Network for phoneme recognition
-# phoneme
--- phoneme_test.m
+++ phoneme_test.m
@@ -6,17 +6,20 @@ function [res] = phoneme_test(in,out)
    fp=fopen("F:\project\Speech Data\phonemix1024.cpp");
    weight = [];
    dat = fgetl(fp);
-   while(dat(1) ~= '.')
+   while(dat ~= -1)
       if(dat(1) == '"')
+          disp("Iteration begin for ");
+          disp(dat);
           dat = dat(2:(length(dat)-1));
           weight = importdata(string(dat));
       elseif (dat(1) == '#')
            disp("Starting");
+      elseif (dat(1) == '.')
+          disp("Iteration ending");
       else
           A = sscanf(dat,'%ld');
           start_id = A(1) / 100000 + 1;
           end_id = A(2) / 100000 + 1;
-          disp(dat);
           disp(start_id);
           disp(end_id);
           weight(start_id:end_id,:) = NeuralNet(input,weight(start_id:end_id,:),out);
