840063dfb9be097de16e7a857f0477c05bdd70ed
Muhammad Z Ilahee <milahee@uber.com>
2018-02-28 23:35:05 -0800

    parallelism added

--- phoneme_test.m
+++ phoneme_test.m
@@ -1,53 +1,47 @@
 % Single iteration of updating the weight
 % This function takes 2  arguments
 % This function outputs the final weight of that iteration
-function [res] = phoneme_test(in,file,phn)   % Function declaration 
+function [res] = phoneme_test(line_id)   % Function declaration 
 % Takes 2 inputs => in is the input filename, out is the output vector
 % Returns updated weight
-   out = [1 0];
-   filematch = 1;
-   input = importdata(in); % Parses input file and gets the input matrix
-   fp=fopen("F:\project\Speech Data\phonemix1024.cpp"); % Opens weight file
-   weight = []; % Creates empty weight
-   dat = fgetl(fp); % Reads the weight file, line by line
-    % while (dat(1) ~= ".")  % Checks for the end of chunk
-    counter = 0;
-    while(dat ~= -1) % Reads till the end
-    %while(counter < 7) %%%%%%%%%%%%%%%%%%%%%%%%%%%% change value from 20 %%%%%%%
-      if(dat(1) == '"')
-          filematch = 1;
-          if (size(strfind(dat,file)) ==0)
-              filematch = 0;
-          else
-              disp("Iteration begin for ");
-              disp(dat);
-              dat = dat(2:(length(dat)-1));
-              weight = importdata(string(dat));
-          end
-      elseif (dat(1) == '#' && filematch)
-           disp("Starting");
-      elseif (dat(1) == '.' && filematch)
-          counter = counter + 1;
-          disp(counter);
-          disp("Iteration ending");
-          s = strcat('F:\project\Speech Data\OUTPUT\file', num2str(counter));
-          s = strcat(s,'.mfc');
-          % csvwrite(s,weight);
-          dlmwrite(s, weight, 'delimiter', '\t', 'precision', '%3.6f')
-          weight = [];
-      elseif (filematch)
-          A = sscanf(dat,'%ld');
-          start_id = A(1) / 100000 + 1;
-          end_id = A(2) / 100000 + 1;
-          disp(start_id);
-          disp(end_id);
-          if(size(strfind(dat,phn)) ~=0)
-            weight(start_id:end_id,:) = NeuralNet(input,weight(start_id:end_id,:),out);
-          end
-          
-      end
+  out = [1 0];
+  weight = importdata("weight.mfc"); % Parses input file and gets the input matrix
+  fp=fopen("Speech Data/phonemix1024.cpp"); % Opens weight file
+  gp="sp aa ch ey ow r n g uh l ax m b ae d iy k er ih dh t p sh ao s th ng eh v nh oy uw hh f chh ay ah dha gh z kh aw h i zh jh j ky y w bh u sil";
+  phn = strsplit(gp)(1,line_id);
+  if(line_id == 54)
+    line_id = 53;
+  end
+  input = []; % Creates empty weight
+  dat = fgetl(fp); % Reads the weight file, line by line
+
+  % while (dat(1) ~= ".")  % Checks for the end of chunk
+  counter = 0;
+  while(dat ~= -1 && counter < 3) % Reads till the end
+    if(dat(1) == '"')
+      disp("Iteration begin for ");
+      disp(dat);
+      dat = dat(2:(length(dat)-1));
+      input = importdata(dat);
+      counter = counter + 1;
+    elseif(dat(1) == '#' || dat(1) == '.')
       dat = fgetl(fp);
+      continue;
+    else(size(strsplit(dat))(2) > 3) 
+      k = strsplit(dat)(1,3);
+      % disp('Comparing '), disp(phn), disp(' to '), disp(k);
+      s =strcmp(k,phn);
+      if(s ~= 0)
+      A = sscanf(dat,'%ld');
+      start_id = A(1) / 100000 + 1;
+      end_id = A(2) / 100000 + 1;
+      weight(line_id,:) = NeuralNet(input(start_id:end_id,:),weight(line_id,:),out);
+      % disp(weight(line_id,:));
+      end
     end
-       fclose(fp);
-   res = weight;
+    dat = fgetl(fp);
+    fflush(stdout);
+  end
+  fclose(fp);
+  res = weight(line_id,:);
 end 
