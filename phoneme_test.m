3a5b6e20eef92e053792245dbb4fb4f865d0bd3c
Muhammad Z Ilahee <milahee@uber.com>
2018-03-01 00:27:23 -0800

    updated phoneme test

--- phoneme_test.m
+++ phoneme_test.m
@@ -9,7 +9,7 @@ function [res] = phoneme_test(line_id)   % Function declaration
   fp=fopen("Speech Data/phonemix1024.cpp"); % Opens weight file
   gp="sp aa ch ey ow r n g uh l ax m b ae d iy k er ih dh t p sh ao s th ng eh v nh oy uw hh f chh ay ah dha gh z kh aw h i zh jh j ky y w bh u sil";
   phn = strsplit(gp)(1,line_id);
-  if(line_id == 54)
+  if(line_id == 54);
     line_id = 53;
   end
   input = []; % Creates empty weight
@@ -17,21 +17,21 @@ function [res] = phoneme_test(line_id)   % Function declaration
 
   % while (dat(1) ~= ".")  % Checks for the end of chunk
   counter = 0;
-  while(dat ~= -1 && counter < 3) % Reads till the end
-    if(dat(1) == '"')
+  while(dat ~= -1 && counter < 3001); % Reads till the end
+    if(dat(1) == '"');
       disp("Iteration begin for ");
       disp(dat);
       dat = dat(2:(length(dat)-1));
       input = importdata(dat);
       counter = counter + 1;
-    elseif(dat(1) == '#' || dat(1) == '.')
+    elseif(dat(1) == '#' || dat(1) == '.');
       dat = fgetl(fp);
       continue;
-    else(size(strsplit(dat))(2) > 3) 
+    else(size(strsplit(dat))(2) > 3); 
       k = strsplit(dat)(1,3);
       % disp('Comparing '), disp(phn), disp(' to '), disp(k);
       s =strcmp(k,phn);
-      if(s ~= 0)
+      if(s ~= 0);
       A = sscanf(dat,'%ld');
       start_id = A(1) / 100000 + 1;
       end_id = A(2) / 100000 + 1;
